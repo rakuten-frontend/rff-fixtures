5e25db21319876ca8a111edfa511024362cc6279
Tanjo, Hiroyuki <expheno@gmail.com>
2014-10-30 17:55:26 +0900

    Install bootstrap and moment.js

--- app/_coffee/main.coffee
+++ app/_coffee/main.coffee
@@ -1,1 +1,11 @@
-console.log 'Welcome to rff-fixtures!'
+do (window = window) ->
+  'use strict'
+
+  $ = window.jQuery
+  moment = window.moment
+
+  renderMoment = ->
+    $('.moment').text(moment().format('YYYY-MM-DD HH:mm:ss'))
+    setTimeout renderMoment, 1000
+
+  renderMoment()
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
-console.log('Welcome to rff-fixtures!');
+(function (window) {
+
+    'use strict';
+
+    var $ = window.jQuery;
+    var moment = window.moment;
+
+    function renderMoment() {
+        $('.moment').text(moment().format('YYYY-MM-DD HH:mm:ss'));
+        setTimeout(renderMoment, 1000);
+    }
+
+    renderMoment();
+
+}(window));
