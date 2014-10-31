3b507716a81fc93ae7c62fefd686a1aa97be41cb
Tanjo, Hiroyuki <expheno@gmail.com>
2014-10-31 13:36:41 +0900

    Add modernizr test

--- app/_coffee/main.coffee
+++ app/_coffee/main.coffee
@@ -2,6 +2,7 @@ do (window = window) ->
   'use strict'
 
   $ = window.jQuery
+  Modernizr = window.Modernizr
   moment = window.moment
 
   renderMoment = ->
@@ -9,3 +10,11 @@ do (window = window) ->
     setTimeout renderMoment, 1000
 
   renderMoment()
+
+  $('.localstorage-support')
+    .text 'Your browser supports localStorage!' if Modernizr.localstorage
+  $('.history-support')
+    .text 'Your browser supports History API!' if Modernizr.history
+  $('.touch-support')
+    .text 'Your browser supports Touch Events!' if Modernizr.touch
+
--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@
     'use strict';
 
     var $ = window.jQuery;
+    var Modernizr = window.Modernizr;
     var moment = window.moment;
 
     function renderMoment() {
@@ -11,5 +12,15 @@
     }
 
     renderMoment();
+
+    if (Modernizr.localstorage) {
+        $('.localstorage-support').text('Your browser supports localStorage!');
+    }
+    if (Modernizr.history) {
+        $('.history-support').text('Your browser supports History API!');
+    }
+    if (Modernizr.touch) {
+        $('.touch-support').text('Your browser supports Touch Events!');
+    }
 
 }(window));
