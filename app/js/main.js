6cafa8e26cd538408081a6fd1f139a9006481311
Tanjo, Hiroyuki <expheno@gmail.com>
2015-01-22 10:54:55 +0900

    Fix indentation of JS file

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,26 @@
 (function (window) {
 
-    'use strict';
+  'use strict';
 
-    var $ = window.jQuery;
-    var Modernizr = window.Modernizr;
-    var moment = window.moment;
+  var $ = window.jQuery;
+  var Modernizr = window.Modernizr;
+  var moment = window.moment;
 
-    function renderMoment() {
-        $('.moment').text(moment().format('YYYY-MM-DD HH:mm:ss'));
-        setTimeout(renderMoment, 1000);
-    }
+  function renderMoment() {
+    $('.moment').text(moment().format('YYYY-MM-DD HH:mm:ss'));
+    setTimeout(renderMoment, 1000);
+  }
 
-    renderMoment();
+  renderMoment();
 
-    if (Modernizr.localstorage) {
-        $('.localstorage-support').text('Your browser supports localStorage!');
-    }
-    if (Modernizr.history) {
-        $('.history-support').text('Your browser supports History API!');
-    }
-    if (Modernizr.touch) {
-        $('.touch-support').text('Your browser supports Touch Events!');
-    }
+  if (Modernizr.localstorage) {
+    $('.localstorage-support').text('Your browser supports localStorage!');
+  }
+  if (Modernizr.history) {
+    $('.history-support').text('Your browser supports History API!');
+  }
+  if (Modernizr.touch) {
+    $('.touch-support').text('Your browser supports Touch Events!');
+  }
 
 }(window));
